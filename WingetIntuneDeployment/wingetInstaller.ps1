983fd25e79a0ee21df3916b15ec76334dcc79a03
Max Heaysman <max.heaysman@ruraldoc.com.au>
2025-05-28 13:42:28 +0930

    Tidied script

--- WingetIntuneDeployment/wingetInstaller.ps1
+++ WingetIntuneDeployment/wingetInstaller.ps1
@@ -1,4 +1,5 @@
 #Winget Intune Deployment
+#Script from https://github.com/mheaysman/Intune-Scripts 
 <# Example Usage:
 .\wingetInstaller.ps1 -scope User -packageName Adobe.Acrobat 
 .\wingetInstaller.ps1 -scope Machine -packageName Adobe.Acrobat -mode uninstall
@@ -9,45 +10,62 @@ param(
     [string]$scope = "Machine",
     [string]$mode = "install"
 )
+$logsfolder = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+$dateTime = Get-Date -Format "yyyyMMdd-HHmmss"
 
-Write-Host "Find the latest version of this script and more at https://github.com/mheaysman/Intune-Scripts"
+Start-Transcript -Path "$($logsfolder)\Winget_$($packageName)_$($mode)_$($dateTime).log" -Append
+Write-Host "Checking provided parameters are valid"
 
-$dateTime = Get-Date -Format "yyyy-MM-dd-HHmmss"
-
-Start-Transcript -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Winget_$($packageName)_$($mode)_$($dateTime).log" -Append
-
-#Stop process if an invalid mode has been selected.
-if ($mode -ne "install" -And $mode -ne "uninstall"){
+Write-Host "Mode Selected: $($mode)"
+if ($mode -eq "install" -Or $mode -eq "uninstall"){
+    Write-Host "Selected mode is valid"
+}
+else{
     Write-Host "Invalid value '$($mode)' provided for parameter 'mode'"
+    Write-Host "Value must be 'install', 'uninstall' or not provided (default install)"
     Write-Host "Exiting..."
-    Stop-Transcript
-    Exit 1
+    Exit 87
 }
 
-#Attempt to find the Folder containing Winget.exe and navigate there
-try {
-    $wingetFolder = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-    Set-Location $wingetFolder[-1].Path
+Write-Host "Scope Selected: $($scope)"
+if ($mode -eq "Machine"){
+    Write-Host "Winget "
+    
+    Write-Host "Attempting to find Winget"
+    try {
+        $wingetFolder = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
+        Set-Location $wingetFolder[-1].Path
+        $cmdlet = ".\winget.exe"
+    }
+    catch {
+        Throw "Error finding Winget folder. Make sure winget is installed on this device"
+        Write-Host "Exiting..."
+        Exit 1601
+    }
+    Write-Host "Winget folder found: $($wingetFolder[-1].Path)"
+
 }
-catch {
-    Throw "Error finding Winget folder. Make sure winget is installed on this device"
+elseif ($mode -eq "User") {
+    $cmdlet = "winget"
+}
+else { 
+    Write-Host "Invalid value '$($scope)' provided for parameter 'scope'"
+    Write-Host "Value must be 'User', 'Machine' or not provided (default Machine)"
     Write-Host "Exiting..."
-    Stop-Transcript
-    Exit 1
+    Exit 87
 }
 
-#install/uninstall the selected package
+Write-Host "preparing to $($mode) $($packageName)"
 try {
-    Write-Host "preparing to $($mode) $($packageName)"
-    .\winget.exe $mode $packageName --silent --accept-source-agreements --accept-package-agreements --Scope $scope
-    Write-Host "Completed $($mode) of $($packageName)"
+    $command = "$($cmdlet) $($mode) $($packageName) --silent --accept-source-agreements --accept-package-agreements --Scope $($scope)"
+    Invoke-Expression $command
 }
 catch {
     Throw "Error while attempting to $($mode) $($packageName)"
     Write-Host "Exiting..."
-    Stop-Transcript
-    Exit 1
+    Exit 1603
 }
 
-Stop-Transcript
+Write-Host "$($mode) of $($packageName) complete"
+Write-Host "Exiting..."
 Exit 0
